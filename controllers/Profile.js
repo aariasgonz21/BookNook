dcc0ef69daeb0abf80f08cd726bcd98e1d0b7533
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2016-12-02 12:40:37 -0500

    Adding Controllers
    Login.js & profile.js

---
+++ controllers/Profile.js
@@ -0,0 +1,17 @@
+//BookNook Profile Controller--Based on the ctp microblog
+
+const express = require('express');
+const Redirect = require('../middlewares/redirect');
+
+module.exports = {
+  registerRouter() {
+    const router = express.Router();
+
+    router.get('/', Redirect.ifNotLoggedIn(), this.index);
+
+    return router;
+  },
+  index(req, res) {
+    res.render('profile', { user: req.user, success: req.flash('success') });
+  },
+};
---
+++ controllers/login.js
@@ -0,0 +1,29 @@
+ //BookNook Login Controller--based on the microblog
+ const express = require('express'); 
+ const passport = require('../middlewares/authentication'); 
+ const Redirect = require('../middlewares/redirect'); 
+ 
+ 
+ module.exports = { 
+   registerRouter() { 
+     const router = express.Router(); 
+ 
+ 
+     router.get('/', Redirect.ifLoggedIn('/profile'), this.index); 
+     router.post('/', this.login); 
+ 
+ 
+     return router; 
+   }, 
+   index(req, res) { 
+     res.render('login', { error: req.flash('error') }); 
+   }, 
+   login(req, res) { 
+     passport.authenticate('local', { 
+       successRedirect: '/profile', 
+       failureRedirect: '/', //should it not go back to home? 
+       failureFlash: true, 
+       successFlash: true, 
+    })(req, res); 
+  }, 
+}; 
